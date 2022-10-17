8f6242d9828b4ea21f2d41416513c5039baa722f
Daniel Wykerd <danielwykerd@gmail.com>
2022-10-17 11:04:42 +0200

    feat(hls): add HLSAudio component

--- packages/hls/src/index.tsx
+++ packages/hls/src/index.tsx
@@ -1,8 +1,8 @@
 import React, { ComponentProps, forwardRef, useEffect, useRef } from "react";
 import Hls from 'hls.js';
-import { Video } from "@react-av/core";
+import { Video, Audio } from "@react-av/core";
 
-const HLSVideo = forwardRef<HTMLVideoElement, ComponentProps<typeof Video> & { src: string }>(function HLSVideo({ src, ...props }, f_ref) {
+export const HLSVideo = forwardRef<HTMLVideoElement, ComponentProps<typeof Video> & { src: string }>(function HLSVideo({ src, ...props }, f_ref) {
     const ref = useRef<HTMLVideoElement>(null);
 
     useEffect(() => {
@@ -14,10 +14,11 @@ const HLSVideo = forwardRef<HTMLVideoElement, ComponentProps<typeof Video> & { s
             const hls = new Hls();
             hls.loadSource(src);
             hls.attachMedia(video);
+            return () => hls.destroy();
         } else {
             // TODO: fallback
         }
-    }, []);
+    }, [src]);
 
     return <Video {...props} ref={current => {
         if (typeof f_ref === "function") f_ref(current);
@@ -28,4 +29,29 @@ const HLSVideo = forwardRef<HTMLVideoElement, ComponentProps<typeof Video> & { s
     }} />;
 });
 
-export default HLSVideo;
+export const HLSAudio = forwardRef<HTMLAudioElement, ComponentProps<typeof Audio> & { src: string }>(function HLSAudio({ src, ...props }, f_ref) {
+    const ref = useRef<HTMLAudioElement>(null);
+
+    useEffect(() => {
+        const audio = ref.current;
+        if (!audio) return;
+        if (audio.canPlayType('application/vnd.apple.mpegurl')) {
+            audio.src = src;
+        } else if (Hls.isSupported()) {
+            const hls = new Hls();
+            hls.loadSource(src);
+            hls.attachMedia(audio);
+            return () => hls.destroy();
+        } else {
+            // TODO: fallback
+        }
+    }, [src]);
+
+    return <Audio {...props} ref={current => {
+        if (typeof f_ref === "function") f_ref(current);
+        // @ts-ignore
+        else if (f_ref) f_ref.current = current;
+        // @ts-ignore
+        ref.current = current;
+    }} />
+});
