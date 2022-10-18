75e15e0e7be2ecaef3def10763c4368b700028e5
Daniel Wykerd <danielwykerd@gmail.com>
2022-10-18 11:57:08 +0200

    feat(hls): useMediaHLS hook

--- packages/hls/src/index.tsx
+++ packages/hls/src/index.tsx
@@ -1,20 +1,42 @@
 import React, { ComponentProps, forwardRef, useEffect, useRef } from "react";
 import Hls from 'hls.js';
-import { Video, Audio } from "@react-av/core";
+import { Video, Audio, useMediaOpaque } from "@react-av/core";
 
-export const HLSVideo = forwardRef<HTMLVideoElement, ComponentProps<typeof Video> & { src: string }>(function HLSVideo({ src, ...props }, f_ref) {
+export interface HLSContext {
+    instance?: Hls;
+    isNative: boolean;
+}
+
+export function useMediaHLS() {
+    const [hls] = useMediaOpaque<HLSContext>('react-av:hls');
+    return hls;
+}
+
+export const HLSVideo = forwardRef<HTMLVideoElement, ComponentProps<typeof Video> & { src: string }>(function HLSVideo({ src, children, ...props }, f_ref) {
     const ref = useRef<HTMLVideoElement>(null);
+    const [, setHLS] = useMediaOpaque<HLSContext>('react-av:hls');
 
     useEffect(() => {
         const video = ref.current;
         if (!video) return;
         if (video.canPlayType('application/vnd.apple.mpegurl')) {
             video.src = src;
+            setHLS({
+                isNative: true
+            });
         } else if (Hls.isSupported()) {
             const hls = new Hls();
             hls.loadSource(src);
             hls.attachMedia(video);
-            return () => hls.destroy();
+            setHLS({
+                instance: hls,
+                isNative: false
+            });
+            return () => {
+                // TODO: first class support for resetting value
+                setHLS(undefined as any);
+                hls.destroy();
+            };
         } else {
             // TODO: fallback
         }
@@ -26,22 +48,36 @@ export const HLSVideo = forwardRef<HTMLVideoElement, ComponentProps<typeof Video
         else if (f_ref) f_ref.current = current;
         // @ts-ignore
         ref.current = current;
-    }} />;
+    }}>
+        {children}
+    </Video>;
 });
 
-export const HLSAudio = forwardRef<HTMLAudioElement, ComponentProps<typeof Audio> & { src: string }>(function HLSAudio({ src, ...props }, f_ref) {
+export const HLSAudio = forwardRef<HTMLAudioElement, ComponentProps<typeof Audio> & { src: string }>(function HLSAudio({ src, children, ...props }, f_ref) {
     const ref = useRef<HTMLAudioElement>(null);
+    const [, setHLS] = useMediaOpaque<HLSContext>('react-av:hls');
 
     useEffect(() => {
         const audio = ref.current;
         if (!audio) return;
         if (audio.canPlayType('application/vnd.apple.mpegurl')) {
             audio.src = src;
+            setHLS({
+                isNative: true
+            });
         } else if (Hls.isSupported()) {
             const hls = new Hls();
             hls.loadSource(src);
             hls.attachMedia(audio);
-            return () => hls.destroy();
+            setHLS({
+                instance: hls,
+                isNative: false
+            });
+            return () => {
+                // TODO: first class support for resetting value
+                setHLS(undefined as any);
+                hls.destroy();
+            };
         } else {
             // TODO: fallback
         }
@@ -53,5 +89,7 @@ export const HLSAudio = forwardRef<HTMLAudioElement, ComponentProps<typeof Audio
         else if (f_ref) f_ref.current = current;
         // @ts-ignore
         ref.current = current;
-    }} />
+    }}>
+        {children}
+    </Audio>
 });
