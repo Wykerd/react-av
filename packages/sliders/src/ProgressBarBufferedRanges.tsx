9d482e0c3564b92fda8c218a190ed0c33415ecdb
Daniel Wykerd <danielwykerd@gmail.com>
2022-10-20 20:40:42 +0200

    fix: buffered ranges include only current
    only include the current buffered range by default

--- packages/sliders/src/ProgressBarBufferedRanges.tsx
+++ packages/sliders/src/ProgressBarBufferedRanges.tsx
@@ -1,9 +1,12 @@
 import React, { HTMLProps, ReactNode } from "react";
-import { useMediaBuffered, useMediaDuration } from "@react-av/core";
+import { useMediaBuffered, useMediaCurrentTime, useMediaDuration } from "@react-av/core";
 
-export default function ProgressBarBufferedRanges({style, ...props}: HTMLProps<HTMLSpanElement>) {
+export default function ProgressBarBufferedRanges({style, all, ...props}: HTMLProps<HTMLSpanElement> & {
+    all?: boolean,
+}) {
     const buffered = useMediaBuffered();
     const duration = useMediaDuration();
+    const [ currentTime ] = useMediaCurrentTime();
 
     const ranges: ReactNode[] = [];
 
@@ -11,7 +14,9 @@ export default function ProgressBarBufferedRanges({style, ...props}: HTMLProps<H
         const start = buffered.start(i);
         const end = buffered.end(i);
 
-        ranges.push(<span key={i} {...props} style={{...style, left: `${(start / duration) * 100}%`, width: `${((end - start) / duration) * 100}%`}}/>);
+        // if not all or if the range includes the current time
+        if (all || (start <= currentTime && end > currentTime)) 
+            ranges.push(<span key={i} {...props} style={{...style, left: `${(start / duration) * 100}%`, width: `${((end - start) / duration) * 100}%`}}/>);
     }
 
     return <>
