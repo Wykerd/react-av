515b4e416631c4b23511ab7220b9280a56e3c0e4
Daniel Wykerd <danielwykerd@gmail.com>
2022-11-02 16:39:26 +0200

    fix(docs): remove translate from video example

--- apps/docs/src/components/VideoExample.tsx
+++ apps/docs/src/components/VideoExample.tsx
@@ -14,7 +14,7 @@ export default function VideoExample() {
         </div>
         <Track kind="subtitles" language="en" label="English" src="./sprite-fright.vtt" id="default" />
         <Media.Viewport className="absolute top-0 bottom-0 left-0 right-0 w-full h-full z-10" inactiveClassName="cursor-none">
-            <InterfaceOverlay className="absolute bottom-0 w-full bg-black/50 p-2 z-10 flex flex-row items-center gap-2 transition cursor-default" inactiveClassName="translate-y-full opacity-0">
+            <InterfaceOverlay className="absolute bottom-0 w-full bg-black/50 p-2 z-10 flex flex-row items-center gap-2 transition cursor-default" inactiveClassName="opacity-0">
                 <PlayPause 
                     className="p-1 bg-transparent focus-visible:bg-red-500 hover:bg-red-500 rounded-md block text-white transition" 
                     defaultIconSize={21} 
